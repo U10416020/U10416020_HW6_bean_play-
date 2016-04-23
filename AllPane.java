7e334721b3753055e802ff39fff70f3b81a1b89d
U10416020 <wkps19961015@gmail.com>
2016-04-24 00:21:17 +0800

    Change the circle
    Use array to set the circles.

--- AllPane.java
+++ AllPane.java
@@ -12,15 +12,17 @@ public  class AllPane extends Pane{
         lineH.setStrokeWidth(3);
         
         Line lineV[] = new Line[11];
-	for(int i =0;i<9;i++){
-            lineV[i] = new Line(90+i*40,400,90+i*40,320);
-            lineV[i].setStrokeWidth(3);
-            getChildren().add(lineV[i]);
+		for(int i =0;i<9;i++){
+				lineV[i] = new Line(90+i*40,400,90+i*40,320);
+				lineV[i].setStrokeWidth(3);
+				getChildren().add(lineV[i]);
         }	
         
         lineV[9] = new Line(230, 100, 230, 70);
+        lineV[9].setStrokeWidth(3);
         lineV[10] = new Line(270, 100, 270, 70);        
-
+        lineV[10].setStrokeWidth(3);
+        
         Line lineS1 = new Line(90, 320, 230, 100);
         lineS1.setStrokeWidth(3);
 	
@@ -31,58 +33,41 @@ public  class AllPane extends Pane{
         getChildren().addAll(lineH, lineS1, lineS2,lineV[9],lineV[10]);
 		
         //Create and set seven circle
-        Circle circle7_1 = new Circle(130, 310, 10);
-        Circle circle7_2 = new Circle(170, 310, 10);
-        Circle circle7_3 = new Circle(210, 310, 10);
-        Circle circle7_4 = new Circle(250, 310, 10);
-        Circle circle7_5 = new Circle(290, 310, 10);
-        Circle circle7_6 = new Circle(330, 310, 10);
-        Circle circle7_7 = new Circle(370, 310, 10);
+		Circle circle7[] = new Circle[7];
+		for(int i =0; i<7;i++){
+			circle7[i] = new Circle(130+i*40,310,10);
+			getChildren().add(circle7[i]);
+		}
 		
-        //Add seven circle to the pane
-        getChildren().addAll(circle7_1, circle7_2, circle7_3, circle7_4,
-        circle7_5, circle7_6, circle7_7);
-	
         //Create and set six circle
-        Circle circle6_1 = new Circle(150, 280, 10);
-        Circle circle6_2 = new Circle(190, 280, 10);
-        Circle circle6_3 = new Circle(230, 280, 10);
-        Circle circle6_4 = new Circle(270, 280, 10);
-        Circle circle6_5 = new Circle(310, 280, 10);
-        Circle circle6_6 = new Circle(350, 280, 10);    		
-		
-		//Add six circle to the pane
-		getChildren().addAll(circle6_1, circle6_2, circle6_3, circle6_4, 
-        circle6_5, circle6_6);		
-		
+		Circle circle6[] = new Circle[6];
+		for(int i =0; i<6;i++){
+			circle6[i] = new Circle(150+i*40,280,10);
+			getChildren().add(circle6[i]);
+		}
+			
 		//Create and set five circle
-		Circle circle5_1 = new Circle(170, 250, 10);
-		Circle circle5_2 = new Circle(210, 250, 10);
-		Circle circle5_3 = new Circle(250, 250, 10);
-		Circle circle5_4 = new Circle(290, 250, 10);				
-		Circle circle5_5 = new Circle(330, 250, 10);
-	
-        //Add five circle to the pane
-		getChildren().addAll(circle5_1, circle5_2, circle5_3, circle5_4, circle5_5);
-	
-		//Create and set four circle
-		Circle circle4_1 = new Circle(190, 220, 10);
-		Circle circle4_2 = new Circle(230, 220, 10);
-		Circle circle4_3 = new Circle(270, 220, 10);
-		Circle circle4_4 = new Circle(310, 220, 10);			
+		Circle circle5[] = new Circle[5];
+		for(int i =0; i<5;i++){
+			circle5[i] = new Circle(170+i*40,250,10);
+			getChildren().add(circle5[i]);
+		}
 		
-		//Add four circle to the pane
-		getChildren().addAll(circle4_1, circle4_2, circle4_3, circle4_4);
-	
-        //Create and set three circle
-		Circle circle3_1 = new Circle(210, 190, 10);
-		Circle circle3_2 = new Circle(250, 190, 10);
-		Circle circle3_3 = new Circle(290, 190, 10);	
-	
-		//Add three circle to the pane
-		getChildren().addAll(circle3_1, circle3_2, circle3_3);
-        
-		//Create and set two circle
+		//Create and set four circle
+		Circle circle4[] = new Circle[4];
+		for(int i =0; i<4;i++){
+			circle4[i] = new Circle(190+i*40,220,10);
+			getChildren().add(circle4[i]);
+		}
+		
+		//Create and set three circle
+		Circle circle3[] = new Circle[3];
+		for(int i =0; i<3;i++){
+			circle7[i] = new Circle(210+i*40,190,10);
+			getChildren().add(circle7[i]);
+		}
+		
+		//Create and set two circle		
 		Circle circle2_1 = new Circle(230, 160, 10);
 		Circle circle2_2 = new Circle(270, 160, 10);				
 		
